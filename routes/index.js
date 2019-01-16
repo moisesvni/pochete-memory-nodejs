7805406791f62fa932d25d6d0265d7d7eb54dab9
Moises Andriao <moisesandriao@duckslab.com.br>
2019-01-16 19:46:24 -0200

    projeto start nodejs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 router.get('/', function (req, res, next) {
     res.status(200).send({
-        title: "Node Express API",
+        title: "PocheteMemory API",
         version: "0.0.1"
     });
 });
