40b3c8106b065e434e73c1a9e0b27b8328496312
Moises Andriao <moisesandriao@duckslab.com.br>
2019-03-19 21:14:01 -0300

    atualizacao cors

--- apps.js
+++ apps.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
+
+const whitelist = ["https://account.dwoom.com", "http://localhost"]
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log(origin)
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true,
+    enablePreflight: true
+}
     
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,5 +25,9 @@ const index = require('./routes');
 const fornecedorRoute = require('./routes/fornecedorRoute');
 app.use('/', index);
 app.use('/fornecedor', fornecedorRoute);
+
+
+app.use(cors(corsOptions))
+app.options("*", cors(corsOptions))
 
 module.exports = app;
